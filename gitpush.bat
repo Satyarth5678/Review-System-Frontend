42aac4b11862925b7257c70e5ac824222b801a82
Satyarth5678 <satyarth.singh636@gmail.com>
2026-05-28 15:24:55 +0530

    Remove temp gitpush.bat

--- gitpush.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-git add .
-git commit -m "Initial commit: Lexa AI landing page"
-git branch -M main
-git remote add origin https://github.com/Satyarth5678/Review-System-Fronted.git
-git push -u origin main

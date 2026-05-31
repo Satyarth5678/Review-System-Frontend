df717c40bde624254413419db97c8bbc2e1f8d90
Satyarth5678 <satyarth.singh636@gmail.com>
2026-05-31 12:46:51 +0530

    fix: react plugin, HashRouter, relative favicon for GitHub Pages

--- gitpush.bat
+++ gitpush.bat
@@ -1,8 +1,8 @@
 @echo off
 git add .
-git commit -m "fix: add react plugin, HashRouter and base path for GitHub Pages"
+git commit -m "fix: react plugin, HashRouter, relative favicon for GitHub Pages"
 git push origin explore
 git checkout main
-git merge explore
+git merge explore --no-edit
 git push origin main
 git checkout explore
